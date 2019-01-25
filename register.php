4742dd5d710a7f45fa6e5822df822abd2b2d2ee7
leonardoigor <igor_mendonca@outlook.com.br>
2019-01-25 13:33:35 -0200

    comit1

---
+++ register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+   
+</head>
+<body>
+    <header class="container" !important>
+        
+        
+    </header>
+</body>
+</html>
+
+<?php
+
+$nome=$_POST['name'];
+$senha=$_POST['password'];
+
+acho $nome;
+
+
+
+?>
